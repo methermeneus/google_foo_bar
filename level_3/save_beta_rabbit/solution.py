4b1f5c397694a10c6c3526ec4c33bfc61c809e40
Randy Grant <whollycrapproductions@gmail.com>
2016-06-30 17:09:57 -0400

    That's all, folks. Looks like foobar beat me legit this time. For save_beta_rabbit, my algorithms wound up with pick two of: fast enough, sufficiently memory efficient, actually correct.

--- level_3/save_beta_rabbit/solution.py
+++ level_3/save_beta_rabbit/solution.py
@@ -16,131 +16,56 @@ def answer (food, grid):
         Return smallest possible nonnegative food value AFTER arriving at
         (len(grid),len(grid[0])). If no nonnegative value is possible, return
         -1
-
-        I think it's easiest, actually, to find total food expenditures over
-        the path, then compare expenditure to food.
-
-    Brute-force messed up again. Looks like this time I'm gonna be walking a
-    linked list, like with zombits.
     """
 
-    class Grid:
-        def __init__ (self, grid, maxVal):
-            self.grid = grid
-            self.maxVal = maxVal
-            self.maxY = len (grid) - 1
-            self.maxX = len (grid[0]) - 1
-            self.finalVal = self.search (self.grid, self.maxVal)
+    def maxValue (grid, maxPoss):
+        r = len (grid)
+        c = len (grid[0])
+        maxVals = [[0 for i in range(c)] for j in range (r)]
+        for i in range (0, r):
+            for j in range (0, c):
+                if i == 0 and j == 0:
+                    maxVals[i][j] = grid[i][j]
+                elif i == 0:
+                    maxVals[i][j] = maxVals[i][j-1] + grid[i][j]
+                elif j == 0:
+                    maxVals[i][j] = maxVals[i-1][j] + grid[i][j]
+                else:
+                    greater = max(maxVals[i][j-1], maxVals[i-1][j])
+                    maxVals[i][j] = greater + grid[i][j]
+        ret = -1
+        for i in range(r):
+            for j in range(c):
+                if maxVals[i][j] > 0 and maxVals[i][j] <= maxPoss and maxVals[i][j] > ret:
+                    ret = maxVals[i][j]
+        return ret
 
-        def search (self, grid, maxVal):
-            start = GridNode (grid, 0, 0, maxVal, self.maxY, self.maxX)
-            if not GridNode.instances:
-                return -1
-            else:
-                total = 201
-                for i in GridNode.finalVals:
-                    if i < total:
-                        total = i
-                return total
-
-
-    class GridNode:
-        instances = []
-        finalVals = []
-        def __init__ (self, grid, x, y, currVal, maxY, maxX):
-            self.grid = grid
-            self.x = x
-            self.y = y
-            self.maxX = maxX
-            self.maxY = maxY
-            self.currVal = currVal - grid[y][x]
-            GridNode.instances.append(self)
-            self.checkSelf()
-
-        def __str__ (self):
-            return "GridNode (" + str (self.x) + "," + str (self.y) + ")"
-        def checkSelf (self):
-            if self.currVal < 0:
-                del GridNode.instances[-1]
-                return
-            else:
-                if self.x < self.maxX:
-                    rightChild = GridNode (self.grid, self.x + 1,
-                            self.y, self.currVal, self.maxY, self.maxX)
-                    #if GridNode.instances:
-                        #if GridNode.instances[-1] == rightChild:
-                            #return
-                if self.y < self.maxY:
-                    downChild = GridNode (self.grid, self.x, self.y + 1,
-                            self.currVal, self.maxY, self.maxX)
-                    #return
-            if (self.x == self.maxX) and (self.y == self.maxY):
-                self.finalVals.append (self.currVal)
-
-    path = Grid (grid, food)
-    return path.finalVal
-
-    """
-
-#Time limit exceeded my ass. Does that mean I took too much time to come up
-# with a solution, or that my solution takes too much server time?
-    Brute-force method. This works for the examples and would probably work for
-    verify if I could run it, but foobar returns a "time limit exceeded" error.
-    Server time is expensive, and this version begins by calculating every
-    possible path, so I can see why it doesn't work.
-    from itertools import permutations
-
-    basePath = ''
-    paths = []
-
-    # Generate a path that goes all right, then all left, just to get a base
-    # case.
-    for i in range (len (grid)):
-        basePath += 'd'
-    for i in range (len (grid[0])):
-        basePath += 'r'
-
-    # All possible paths in this scenario have the same number of moves (width
-    # + length - 2). That means that all paths are just permutations of each
-    # other. Let itertools take care of making the permutations...
-    for i in permutations(basePath):
-        if not i in paths:
-            paths.append(i)
-
-    x, y, z = 0, 0, 0
-    pathTotals = [0]
-
-    # Get the amount of food expended in each path.
-    for i in paths:
-        for j in i:
-            if j == 'd' and y < len (grid) - 1:
-                y += 1
-            elif j == 'r' and x < len (grid[0]) - 1:
-                x += 1
-            pathTotals[z] += grid[y][x]
-            # Quick fix to reduce computation time a little. Not enough,
-            # apparently.
-            if pathTotals[z] > food:
-                pathTotals[z], x, y = 0, 0, 0
-                break
-        z += 1
-        pathTotals.append (0)
-        x, y = 0, 0
-
-    # Find 1: Whether or not a solution where food expended < food carried
-    # exists, then 2: the largest amount of food that can be expended within
-    # those paths
-    solution = False
-    currentMax = 0
-    for i in range (0, len(pathTotals)):
-        if pathTotals[i] <= food:
-            solution = True
-            if pathTotals[i] > currentMax:
-                currentMax = pathTotals[i]
-    # And, finally, return -1 if no solution exists, or food remaining if a
-    # solution does exist.
-    if not solution:
+    ret = maxValue(grid, food)
+    if ret == -1:
         return -1
     else:
-        return food - currentMax
+        return food - ret
+
+
+
+    """
+    def search (grid, currVal, x, y):
+        global finalVal
+        currVal -= grid[y][x]
+        if currVal < 0:
+            return
+        if (x == maxX) and (y == maxY) and ((finalVal == 0) or (finalVal == 1)):
+            return
+
+
+        if x < maxX:
+            search (grid, currVal, x + 1, y)
+        if y < maxY:
+            search (grid, currVal, x, y + 1)
+
+        if (x == maxX) and (y == maxY):
+            if (currVal < finalVal) or (finalVal == -1):
+                    finalVal = currVal
+
+    search (grid, food, 0, 0)
     """
